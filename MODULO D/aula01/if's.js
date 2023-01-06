124230cf0cae5ab754780432fa88962dcc7b6d82
viniciustixu <vinicipkt@live.com>
2023-01-06 15:31:36 -0300

    modulo D aula01

---
+++ MODULO D/aula01/if's.js
@@ -0,0 +1,9 @@
+let velocidade = 60
+
+if (velocidade > 60) {
+    console.log('multado')
+} else if (velocidade == 60) {
+     console.log('sua velocidade é 60')
+    } else {
+        console.log('Você está dentro da velocidade permitida')
+    }
